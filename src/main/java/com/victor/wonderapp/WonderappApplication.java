49d979204a09a5afa8f9722209c1d2a7048b1a6c
victorlamprojects <victorlamprojects@gmail.com>
2025-07-27 16:11:33 +0800

    init

---
+++ src/main/java/com/victor/wonderapp/WonderappApplication.java
@@ -0,0 +1,13 @@
+package com.victor.wonderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/wonderapp/WonderappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.wonderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
